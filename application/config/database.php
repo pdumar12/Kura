d932dc66daa6ee9687f1898a8ad2c3e786a4d05c
pdumar12 <85407476+pdumar12@users.noreply.github.com>
2025-06-13 12:34:26 -0500

    Update database.php
    Update v2

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost:',
+	'hostname' => 'localhost',
 	'username' => 'u750923273_kura',
 	'password' => 'Manuelabedoya1*',
 	'database' => 'u750923273_kura',
