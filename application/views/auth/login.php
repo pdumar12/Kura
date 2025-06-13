c7e87daf37da25d2fd4f71fb0c251545ed91edc3
pdumar12 <85407476+pdumar12@users.noreply.github.com>
2025-06-13 12:39:49 -0500

    Add doctor registration link to login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -165,6 +165,9 @@
         <p class="mb-0 text-center">
           <a data-toggle="modal" href="#myModal"><?php echo lang('forgot_your_password') ?>?</a>
         </p>
+        <p class="mb-0 text-center mt-2">
+          <a href="doctor/addNewView"><?php echo lang('doctor_registration_form'); ?></a>
+        </p>
       </div>
       <!-- /.login-card-body -->
     </div>
